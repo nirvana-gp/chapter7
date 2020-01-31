a64a51aebd12b4f1a534fae092a26dc9891fffc3
Nirvana <568639751.qq.com>
2020-01-31 11:58:49 +0800

    首次提交

---
+++ modules/dirlister.py
@@ -0,0 +1,11 @@
+#! python2
+# coding=utf-8
+
+import os
+
+def run(**args):
+    print "[*] In dirlister module"
+    files = os.listdir(".")
+
+    return str(files)
+
---
+++ modules/environment.py
@@ -0,0 +1,9 @@
+#! python2
+# coding=utf-8
+
+import os
+
+def run(**args):
+    print "[*] In environment module"
+    return str(os.environ)
+
